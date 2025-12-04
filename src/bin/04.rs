6b59c6aa55820318539a2d77d3e7d9635482a784
Barr <barr@barrisrael.com>
2025-12-04 14:06:11 +0200

    remove gif

---
+++ src/bin/04.rs
@@ -0,0 +1,185 @@
+use std::{io, time::Duration};
+
+use advent_of_code::util::fast_parse;
+use ratatui::{
+    Frame,
+    buffer::Buffer,
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    layout::{Constraint, Layout, Rect},
+    style::Stylize,
+    text::{Line, Text},
+    widgets::{Block, BorderType, Paragraph, Widget, canvas::Canvas},
+};
+
+advent_of_code::solution!(4);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[derive(Debug)]
+struct Part1App<'a> {
+    input: &'a [u8],
+}
+
+impl<'a> Part1App<'a> {
+    fn reset(&mut self) {
+        *self = Self::new(self.input);
+    }
+
+    fn new(input: &'a [u8]) -> Self {
+        Self {
+            input,
+        }
+    }
+}
+
+fn part_one_tick(_state: &mut Part1App<'_>) {
+}
+
+impl Widget for &Part1App<'_> {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from("Part One".bold());
+        let status = Text::from(format!("Length: {}", self.input.len()));
+        let block = Block::bordered().title(title.centered());
+        let [status_area, dial_area] =
+            Layout::vertical([Constraint::Percentage(10), Constraint::Percentage(90)])
+                .areas(block.inner(area));
+        block.render(area, buf);
+        Canvas::default()
+            .x_bounds([-200f64, 200f64])
+            .y_bounds([-100f64, 100f64])
+            .block(Block::bordered().border_type(BorderType::Double))
+            .paint(|_ctx| {
+            })
+            .render(dial_area, buf);
+        Paragraph::new(status).centered().render(status_area, buf);
+    }
+}
+
+#[derive(Debug)]
+struct Part2App<'a> {
+    input: &'a [u8],
+}
+
+impl<'a> Part2App<'a> {
+    fn reset(&mut self) {
+        *self = Self::new(self.input);
+    }
+
+    fn new(input: &'a [u8]) -> Self {
+        Self {
+            input,
+        }
+    }
+}
+
+fn part_two_tick(_state: &mut Part2App<'_>) {
+}
+
+impl Widget for &Part2App<'_> {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from("Part Two".bold());
+        let status = Text::from(format!("Length: {}", self.input.len()));
+        let block = Block::bordered().title(title.centered());
+        let [status_area, dial_area] =
+            Layout::vertical([Constraint::Percentage(10), Constraint::Percentage(90)])
+                .areas(block.inner(area));
+        block.render(area, buf);
+        Canvas::default()
+            .x_bounds([-200f64, 200f64])
+            .y_bounds([-100f64, 100f64])
+            .block(Block::bordered().border_type(BorderType::Double))
+            .paint(|_ctx| {
+            })
+            .render(dial_area, buf);
+        Paragraph::new(status).centered().render(status_area, buf);
+    }
+}
+
+pub fn part_one_tui(input: &str) -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let mut state = Part1App::new(input.as_bytes());
+    let mut paused = false;
+    loop {
+        terminal.clear()?;
+        part_one_tick(&mut state);
+        terminal.draw(|frame: &mut Frame| {
+            frame.render_widget(&state, frame.area());
+        })?;
+        if !event::poll(Duration::from_millis(8))? && !paused {
+            continue;
+        }
+        if let Event::Key(KeyEvent {
+            code,
+            modifiers: _,
+            kind: KeyEventKind::Press,
+            state: _,
+        }) = event::read()?
+        {
+            match code {
+                KeyCode::Char('q') => {
+                    ratatui::restore();
+                    return Ok(());
+                }
+                KeyCode::Char('r') => state.reset(),
+                KeyCode::Char('p') => paused = !paused,
+                _ => {}
+            }
+        }
+    }
+}
+
+pub fn part_two_tui(input: &str) -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let mut state = Part2App::new(input.as_bytes());
+    let mut paused = false;
+    loop {
+        terminal.clear()?;
+        part_two_tick(&mut state);
+        terminal.draw(|frame: &mut Frame| {
+            frame.render_widget(&state, frame.area());
+        })?;
+        if !event::poll(Duration::from_millis(8))? && !paused {
+            continue;
+        }
+        if let Event::Key(KeyEvent {
+            code,
+            modifiers: _,
+            kind: KeyEventKind::Press,
+            state: _,
+        }) = event::read()?
+        {
+            match code {
+                KeyCode::Char('q') => {
+                    ratatui::restore();
+                    return Ok(());
+                }
+                KeyCode::Char('r') => state.reset(),
+                KeyCode::Char('p') => paused = !paused,
+                _ => {}
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
