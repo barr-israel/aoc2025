669a855742d84fad4384d31e117d8497eee47a51
Barr <barr@barrisrael.com>
2025-12-08 15:27:28 +0200

    faster day 8

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,9 +1,4 @@
-use std::{
-    collections::{BinaryHeap, HashSet},
-    io,
-    mem::swap,
-    time::Duration,
-};
+use std::{collections::HashSet, io, mem::swap, time::Duration};
 
 use advent_of_code::util::fast_parse;
 use ratatui::{
@@ -37,10 +32,10 @@ fn calc_distance(box1: (i32, i32, i32), box2: (i32, i32, i32)) -> u64 {
     let dx = (box1.0 - box2.0) as i64;
     let dy = (box1.1 - box2.1) as i64;
     let dz = (box1.2 - box2.2) as i64;
-    (dx * dx + dy * dy + dz * dz).isqrt() as u64
+    (dx * dx + dy * dy + dz * dz) as u64
 }
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
 struct Distance {
     box1: u32,
     box2: u32,
@@ -55,13 +50,13 @@ impl PartialOrd for Distance {
 
 impl Ord for Distance {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.distance.cmp(&self.distance)
+        self.distance.cmp(&other.distance)
     }
 }
 
-fn boxes_to_distances(boxes: &[(i32, i32, i32)]) -> BinaryHeap<Distance> {
+fn boxes_to_distances(boxes: &[(i32, i32, i32)]) -> Vec<Distance> {
     let distance_count = (boxes.len() * (boxes.len() + 1)) / 2;
-    let mut distances = BinaryHeap::with_capacity(distance_count);
+    let mut distances = Vec::with_capacity(distance_count);
     for (idx1, box1) in boxes.iter().enumerate() {
         for (idx2, box2) in boxes[idx1 + 1..].iter().enumerate() {
             // correct the offset
@@ -73,6 +68,7 @@ fn boxes_to_distances(boxes: &[(i32, i32, i32)]) -> BinaryHeap<Distance> {
             });
         }
     }
+    distances.sort_unstable();
     distances
 }
 
@@ -99,10 +95,10 @@ pub fn part_one(input: &str) -> Option<u64> {
     part_one_inner(input, 1000)
 }
 
-fn part_one_inner(input: &str, to_connect: u32) -> Option<u64> {
+fn part_one_inner(input: &str, to_connect: usize) -> Option<u64> {
     let boxes = parse_input(input.as_bytes());
     let box_count = boxes.len();
-    let mut distances = boxes_to_distances(&boxes);
+    let distances = boxes_to_distances(&boxes);
     let mut box_to_circuit: Vec<u32> = (0..box_count as u32).collect();
     let mut circuits: Vec<_> = (0..box_count as u32)
         .map(|b| {
@@ -111,10 +107,7 @@ fn part_one_inner(input: &str, to_connect: u32) -> Option<u64> {
             s
         })
         .collect();
-    for _ in 0..to_connect {
-        let potential_connection = distances
-            .pop()
-            .expect("There must be at least 1000 possible connections");
+    for potential_connection in &distances[..to_connect] {
         let mut circuit1_id = box_to_circuit[potential_connection.box1 as usize];
         let mut circuit2_id = box_to_circuit[potential_connection.box2 as usize];
         if circuit1_id != circuit2_id {
@@ -140,48 +133,53 @@ fn part_one_inner(input: &str, to_connect: u32) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let boxes = parse_input(input.as_bytes());
-    let box_count = boxes.len();
-    let mut distances = boxes_to_distances(&boxes);
-    let mut box_to_circuit: Vec<u32> = (0..box_count as u32).collect();
-    let mut circuits: Vec<_> = (0..box_count as u32)
-        .map(|b| {
-            let mut s = HashSet::new();
-            s.insert(b);
-            s
-        })
-        .collect();
-    loop {
-        let potential_connection = distances
-            .pop()
-            .expect("There must be at least 1000 possible connections");
-        let mut circuit1_id = box_to_circuit[potential_connection.box1 as usize];
-        let mut circuit2_id = box_to_circuit[potential_connection.box2 as usize];
-        if circuit1_id != circuit2_id {
-            if circuit2_id < circuit1_id {
-                // make id 1 the smallest for the split
-                swap(&mut circuit1_id, &mut circuit2_id);
-            }
-            let (s1, s2) = circuits.split_at_mut(circuit2_id as usize);
-            let mut circuit1 = &mut s1[circuit1_id as usize];
-            let mut circuit2 = &mut s2[0];
-            if circuit2.len() < circuit1.len() {
-                // better to combine smallest into largest, so swap them
-                swap(&mut circuit1, &mut circuit2);
-                swap(&mut circuit1_id, &mut circuit2_id);
-            }
-            circuit1.drain().for_each(|box_id| {
-                box_to_circuit[box_id as usize] = circuit2_id;
-                circuit2.insert(box_id);
-            });
-            if circuit2.len() == box_count {
-                return Some(
-                    boxes[potential_connection.box1 as usize].0 as u64
-                        * boxes[potential_connection.box2 as usize].0 as u64,
-                );
+    let mut n = 0;
+    for _ in 0..100 {
+        let boxes = parse_input(input.as_bytes());
+        let box_count = boxes.len();
+        let distances = boxes_to_distances(&boxes);
+        let mut box_to_circuit: Vec<u32> = (0..box_count as u32).collect();
+        let mut circuits: Vec<_> = (0..box_count as u32)
+            .map(|b| {
+                let mut s = HashSet::new();
+                s.insert(b);
+                s
+            })
+            .collect();
+        for potential_connection in distances {
+            let mut circuit1_id = box_to_circuit[potential_connection.box1 as usize];
+            let mut circuit2_id = box_to_circuit[potential_connection.box2 as usize];
+            if circuit1_id != circuit2_id {
+                if circuit2_id < circuit1_id {
+                    // make id 1 the smallest for the split
+                    swap(&mut circuit1_id, &mut circuit2_id);
+                }
+                let (s1, s2) = circuits.split_at_mut(circuit2_id as usize);
+                let mut circuit1 = &mut s1[circuit1_id as usize];
+                let mut circuit2 = &mut s2[0];
+                if circuit2.len() < circuit1.len() {
+                    // better to combine smallest into largest, so swap them
+                    swap(&mut circuit1, &mut circuit2);
+                    swap(&mut circuit1_id, &mut circuit2_id);
+                }
+                circuit1.drain().for_each(|box_id| {
+                    box_to_circuit[box_id as usize] = circuit2_id;
+                    circuit2.insert(box_id);
+                });
+                if circuit2.len() == box_count {
+                    // return Some(
+                    //     boxes[potential_connection.box1 as usize].0 as u64
+                    //         * boxes[potential_connection.box2 as usize].0 as u64,
+                    // );
+                    n += boxes[potential_connection.box1 as usize].0 as u64
+                        * boxes[potential_connection.box2 as usize].0 as u64;
+                    break;
+                }
             }
         }
+        // unreachable!()
     }
+    Some(n)
 }
 
 #[derive(Debug)]
@@ -225,7 +223,7 @@ struct Part2App<'a> {
     input: &'a [u8],
     boxes: Vec<(i32, i32, i32)>,
     box_count: usize,
-    distances: BinaryHeap<Distance>,
+    distances: Vec<Distance>,
     box_to_circuit: Vec<u32>,
     circuits: Vec<HashSet<u32>>,
     done: bool,
@@ -234,6 +232,7 @@ struct Part2App<'a> {
     max_x: f64,
     min_y: f64,
     max_y: f64,
+    conn_idx: usize,
 }
 
 impl<'a> Part2App<'a> {
@@ -280,6 +279,7 @@ impl<'a> Part2App<'a> {
             min_y,
             max_y,
             done: false,
+            conn_idx: 0usize,
         }
     }
 }
@@ -289,10 +289,8 @@ fn part_two_tick(state: &mut Part2App<'_>) {
         if state.done {
             return;
         }
-        let potential_connection = state
-            .distances
-            .pop()
-            .expect("There must be at least 1000 possible connections");
+        let potential_connection = state.distances[state.conn_idx];
+        state.conn_idx += 1;
         let mut circuit1_id = state.box_to_circuit[potential_connection.box1 as usize];
         let mut circuit2_id = state.box_to_circuit[potential_connection.box2 as usize];
         if circuit1_id != circuit2_id {
@@ -326,7 +324,12 @@ fn part_two_tick(state: &mut Part2App<'_>) {
 impl Widget for &Part2App<'_> {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from("Part Two".bold());
-        let status = Text::from(format!("Done: {}", self.done));
+        let status = Text::from(format!(
+            "Connections Checked {}/{}, Done: {}",
+            self.conn_idx,
+            self.distances.len(),
+            self.done
+        ));
         let block = Block::bordered().title(title.centered());
         let [status_area, dial_area] =
             Layout::vertical([Constraint::Percentage(10), Constraint::Percentage(90)])
